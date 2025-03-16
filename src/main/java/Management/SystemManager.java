f3eaf9d4194bbdcd5a7239ed7668f563cea7c3e7
db1tz3r <147137582+db1tz3r@users.noreply.github.com>
2025-03-16 12:42:49 +0100

    Zwischenspeicher, erkennt jetzt anhand von den Startwerten ob Match und verknüpft dann wenn möglich die Kombinierten Einzelergebnisse

--- src/main/java/Management/SystemManager.java
+++ src/main/java/Management/SystemManager.java
@@ -58,7 +58,8 @@ public class SystemManager {
 
                     List<List<List>> zyklusErgebnis = new ArrayList<>();    //zyklusSpeicher.checkIntervallFromAllInstanzes(lokaleSpeicherung);
                     zyklusSpeicher.addLokaleDaten(lokaleSpeicherung);
-                    zyklusSpeicher.findMatchStartpunkt();
+                    zyklusSpeicher.startMatching();
+                    //System.out.println("Zyklen: " + zyklusSpeicher.getFehlendeDatenSpeicher());
 
                     // Falls `zyklusErgebnis` nicht leer ist, weiterverarbeiten
                     if (zyklusErgebnis != null && !zyklusErgebnis.isEmpty()) {
