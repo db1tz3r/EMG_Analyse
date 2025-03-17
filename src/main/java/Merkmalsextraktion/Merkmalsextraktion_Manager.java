7eb727f3879b6f6545c5b80f3b8c3b6f1df1281b
db1tz3r <147137582+db1tz3r@users.noreply.github.com>
2025-03-17 13:38:39 +0100

    Jetzt auch Sensor ohne match zurückgeben

--- src/main/java/Merkmalsextraktion/Merkmalsextraktion_Manager.java
+++ src/main/java/Merkmalsextraktion/Merkmalsextraktion_Manager.java
@@ -235,7 +235,7 @@ public class Merkmalsextraktion_Manager {
     // Getter und Setter
     // Hauptliste in Steigung, Senkung, Mittel und Gesamt aufteilen
     public ArrayList<Double> getSteigungsArrayGerichtet(List<List<List<Double>>> zyklusErgebnis, int instanzID) {
-        System.out.println("SteigungsArrayGerichtet" + zyklusErgebnis.get(instanzID).get(0));
+//        System.out.println("SteigungsArrayGerichtet" + zyklusErgebnis.get(instanzID).get(0));
         return new ArrayList<>(zyklusErgebnis.get(instanzID).get(0));
     }
 
